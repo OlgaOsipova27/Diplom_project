f43d0689d648ac66ba9566b73326accd40c6ef08
OlgaOsipova27 <opetrova0498@gmail.com>
2025-08-08 14:41:20 +0300

    добавление новых файлов

---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type" : "application/json"
+}
+
+order_body = {
+    "firstName": "Ольга",
+    "lastName": "Осипова",
+    "address": "Бартеневская 18к2",
+    "metroStation": 204,
+    "phone": "+79251295839",
+    "rentTime": 5,
+    "deliveryDate": "2020-08-20",
+    "comment": "Сдаем дипломный проект",
+    "color": [
+        "BLACK"
+    ]
+}
